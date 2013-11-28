a2a834d4a3306f70120c17f01b20ea71c3fd704c
Bayo Erinle <berinle@gmail.com>
2013-11-27 21:47:19 -0500

    prep for redis cloud integration

--- src/main/java/com/myapp/RedisConfiguration.java
+++ src/main/java/com/myapp/RedisConfiguration.java
@@ -3,6 +3,8 @@ package com.myapp;
 import com.myapp.messaging.Receiver;
 import com.myapp.service.EmailService;
 import com.myapp.service.EmailServiceImpl;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -11,15 +13,34 @@ import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * @author berinle
  */
 @Configuration
 public class RedisConfiguration {
 
+    private static final Log log = LogFactory.getLog(RedisConfiguration.class);
+
     @Bean
     JedisConnectionFactory connectionFactory(){
-        return new JedisConnectionFactory();
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+
+        //attempt to read values dynamically
+        try {
+            String rediscloud_url = System.getenv("REDISCLOUD_URL");
+            if(null != rediscloud_url){
+                URI uri = new URI(rediscloud_url);
+                connectionFactory.setHostName(uri.getHost());
+                connectionFactory.setPort(uri.getPort());
+            }
+        } catch (URISyntaxException e) {
+            log.error(e);
+        }
+
+        return connectionFactory;
     }
 
     @Bean
